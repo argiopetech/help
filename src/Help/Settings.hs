d6b56334a9f44c9d2660c20090c4219d5bbffebe
Elliot Robinson <elliot.robinson@argiopetech.com>
2013-05-22 23:42:10 -0400

    Adds Lens Getters for Settings

--- src/Help/Settings.hs
+++ src/Help/Settings.hs
@@ -1,22 +1,44 @@
 {-# LANGUAGE NoImplicitPrelude, UnicodeSyntax #-}
-module Help.Settings ( Settings(..)
+module Help.Settings ( -- *The Settings type
+                       Settings
+                       -- *Lens getters for Settings
+                     , ymlFile
+                     , loadMode
+                     , logFile
+                       -- *Utilities
                      , loadSettings
                      ) where
 
 import Help.Imports
 
 import Control.Applicative
+import Control.Lens.Getter (to, Getter)
 
-data Settings = Settings { ymlFile ∷ Text }
+data Settings = Settings { _ymlFile  ∷ FilePath
+                         , _loadMode ∷ Bool
+                         , _logFile  ∷ FilePath
+                         } deriving (Show)
 
-data TempSettings = TempSettings (Maybe Text)
+ymlFile ∷ Getter Settings Text
+ymlFile = to _ymlFile
+
+loadMode ∷ Getter Settings Bool
+loadMode = to _loadMode
+
+logFile ∷ Getter Settings Text
+logFile = to _logFile
+
+data TempSettings = TempSettings { tempYmlFile  ∷ (Maybe FilePath)
+                                 , tempLoadMode ∷ (Maybe Bool)
+                                 , tempLogFile  ∷ (Maybe FilePath)
+                                 }
 
 -- |Loads all settings and creates one authoritative set
 loadSettings ∷ IO Settings
 loadSettings = do
     args ← getArgs
     let cliSettings = loadCLISettings args
-    ymlSettings     ← loadYMLSettings $ tempYmlFile $ cliSettings `overrides` defaultSettings
+    ymlSettings     ← loadYMLSettings $ loadableYmlFile $ cliSettings `overrides` defaultSettings
     return $ verifySettings $ cliSettings `overrides` ymlSettings `overrides` defaultSettings
 
 -- |Changes a TempSettings to a Settings, assuming it contains all required options
@@ -40,8 +62,9 @@ overrides ∷ TempSettings → TempSettings → TempSettings
 overrides ns os = undefined
 
 -- |Pull the YAML file from TempSettings. In the event that the current stack doesn't have a file, fallback to default.
--- 
+--
 -- IN THE EVENT THAT NO DEFAULT FILE EXISTS, THIS WILL LOOP INFINITELY!
-tempYmlFile ∷ TempSettings → Text
-tempYmlFile (TempSettings (Just t)) = t
-tempYmlFile _                       = tempYmlFile defaultSettings
+loadableYmlFile ∷ TempSettings → Text
+loadableYmlFile s = case tempYmlFile s of
+                      (Just f) → f
+                      Nothing  → loadableYmlFile defaultSettings
